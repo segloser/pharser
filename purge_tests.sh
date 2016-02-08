c88990e289a0bee1d3fdc931d03b7acefcd451f8
Segloser <retraining@segloser.com>
2016-02-08 07:06:45 +0100

    Update purge_tests.sh

--- purge_tests.sh
+++ purge_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "This script will remove the entire pharser directory. This means that you will lose everything. If you want to preserve your last test results, save them before excuting this script"
-read -p "Press [ENTER] if you want to delete the pharser directory or [CTRL+C] to abort this script."
+read -p "Press [ENTER] if you want to delete the pharser directory to restart the installation test or [CTRL+C] to abort this script."
 cd /root
 rm -r pharser
