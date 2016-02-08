77149e63f856b717f158a965e13f75520abdf6da
Segloser <retraining@segloser.com>
2016-02-08 07:01:17 +0100

    Update purge_tests.sh

--- purge_tests.sh
+++ purge_tests.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-rm -r parsed
-rm ./wgot/good/*
-rm ./wgot/bad/*
-rm bad_content_hashed.txt bulk.txt dict* match* good_content_hashed.txt ls_bad.txt ls_good.txt out2.txt output.txt urls_process.txt
-cp ./forced_matches/* ./wgot/bad
+cd /root
+rm -r pharser
